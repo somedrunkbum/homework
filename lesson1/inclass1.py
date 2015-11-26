53a7517eb5d2a0f5336dc954be5b5851242401e0
Sergey Kalikin <s_kalikin@wargaming.net>
2015-11-26 21:11:22 +0300

    lesson1 tasks

---
+++ lesson1/inclass1.py
@@ -0,0 +1,9 @@
+l = [1, 2, 3, 4]
+n = 3
+
+if n > len(l):
+    x = -1
+else:
+    x = l[n] ** n
+
+print x
---
+++ lesson1/inclass2.py
@@ -0,0 +1,8 @@
+l = ["test",  "test", "test2", "test", "test"]
+x = []
+
+for n in l:
+    if n not in x:
+        x.append(n)
+
+print x
